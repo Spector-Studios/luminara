0bcfcfaba3658ff61322b3e8b26bf6ffc29374bf
Ved Prajapati <104439483+Spector-Studios@users.noreply.github.com>
2025-09-16 13:22:07 +0530

    Delete commented code

--- src/state/animation.rs
+++ src/state/animation.rs
@@ -19,12 +19,6 @@ pub struct MoveAnimation {
     path: Vec<Point>,
 }
 
-/* #[derive(Debug)]
-pub struct ShiftMapView {
-    target_rect: Rect,
-    target_tilerect: TileRect,
-} */
-
 impl MoveAnimation {
     pub fn boxed_new(unit: Unit, path: Vec<Point>) -> Box<Self> {
         Box::new(Self {
@@ -81,73 +75,3 @@ impl GameState for MoveAnimation {
         "Move Animation"
     }
 }
-
-/* impl ShiftMapView {
-    pub fn boxed_new(dest: impl Into<Point>, viewport: &Viewport) -> Option<Box<Self>> {
-        const MARGIN: i32 = 2;
-
-        let dest = dest.into();
-        let mut target_rect = viewport.map_view;
-
-        let min_x = dest.x - target_rect.w + MARGIN + 1;
-        let max_x = dest.x - MARGIN;
-        target_rect.x = target_rect.x.clamp(min_x, max_x);
-
-        let min_y = dest.y - target_rect.h + MARGIN + 1;
-        let max_y = dest.y - MARGIN;
-        target_rect.y = target_rect.y.clamp(min_y, max_y);
-
-        target_rect = viewport.clamp_tilerect_to_map(target_rect);
-
-        // TODO Move this logic to the call site
-        if target_rect == viewport.map_view {
-            return None;
-        }
-
-        Some(Box::new(Self {
-            target_rect: target_rect.into(),
-            target_tilerect: target_rect,
-        }))
-    }
-}
-impl GameState for ShiftMapView {
-    fn update(
-        &mut self,
-        _msg_queue: &mut VecDeque<GameMsg>,
-        _commands_buffer: &mut Commands,
-        game_ctx: GameCtxView,
-    ) -> Transition {
-        const STEP_SIZE: f32 = 0.2;
-        const SNAP_TOLERANCE: f32 = 1e-3;
-
-        let mut render_rect = game_ctx.viewport.get_render_rect();
-
-        let dx = self.target_rect.x - render_rect.x;
-        let dy = self.target_rect.y - render_rect.y;
-        if dx.abs() > 0.0 || dy.abs() > 0.0 {
-            let shift_x = dx.signum() * dx.abs().min(STEP_SIZE);
-            let shift_y = dy.signum() * dy.abs().min(STEP_SIZE);
-
-            render_rect.x += shift_x;
-            render_rect.y += shift_y;
-
-            if (render_rect.x - self.target_rect.x).abs() < SNAP_TOLERANCE {
-                render_rect.x = self.target_rect.x;
-            }
-            if (render_rect.y - self.target_rect.y).abs() < SNAP_TOLERANCE {
-                render_rect.y = self.target_rect.y;
-            }
-
-            game_ctx.viewport.render_view = Some(render_rect);
-            return Transition::None;
-        }
-
-        game_ctx.viewport.map_view = self.target_tilerect;
-        game_ctx.viewport.render_view = None;
-        Transition::Pop
-    }
-
-    fn name(&self) -> &'static str {
-        "Shift Map View"
-    }
-} */
